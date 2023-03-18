22382d1a7517a2cb5235f95af2d14d3edaf671eb
hopy <ml.crayssac@gmail.com>
2023-03-19 00:03:01 +0100

    First commit

---
+++ main.pro
@@ -0,0 +1,7 @@
+TEMPLATE=app
+TARGET=window
+QT=core gui widgets
+SOURCES+=window.cpp \
+    main.cpp
+HEADERS+=window.hpp
+RESOURCES     = application.qrc
---
+++ window.hpp
@@ -0,0 +1,42 @@
+#include <QMainWindow>
+#include <QPlainTextEdit>
+#include <QCloseEvent>
+#include <QSessionManager>
+
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow();
+
+    void loadFile(const QString &fileName);
+
+protected:
+    void closeEvent(QCloseEvent *event) override;
+
+private slots:
+    void newFile();
+    void open();
+    bool save();
+    bool saveAs();
+    void about();
+    void documentWasModified();
+#ifndef QT_NO_SESSIONMANAGER
+    void commitData(QSessionManager &);
+#endif
+
+private:
+    void createActions();
+    void createStatusBar();
+    void readSettings();
+    void writeSettings();
+    bool maybeSave();
+    bool saveFile(const QString &fileName);
+    void setCurrentFile(const QString &fileName);
+    QString strippedName(const QString &fullFileName);
+
+    QPlainTextEdit *textEdit;
+    QString curFile;
+};
